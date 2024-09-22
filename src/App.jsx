c89325b58239fb56206edd7757aae6e7c18fbd09
walaazak-CSE <walaazakarneh23@gmail.com>
2024-09-22 12:40:08 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import About from './Components/Abouts/About';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1>Welcome to My React App</h1>
+    <About/>
+    </>
+  )
+
+}
+
+export default App
